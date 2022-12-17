df747a2dc83fce2fb14f101629cb987925805371
Alex Fischman <alexdfischman@gmail.com>
2022-12-17 12:36:14 -0800

    breast, back

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// nose, tongue, breast, wing, arm/hand, neck, hair, leg/foot, horn,
-//	back, egg, knee, skin, thigh, tail, belly, feather, finger/toenail, head, heart, 
+// nose, tongue, wing, arm/hand, neck, hair, leg/foot, horn,
+//	egg, knee, skin, thigh, tail, belly, feather, finger/toenail, head, heart, 
 // to attack, to eat/drink, to stand, to laugh, to hide, to breathe, to tie, to crush/grind,
 // to walk, to run, to sit, to sleep, to carry
 // root, leaf, seed, tree
@@ -10,7 +10,7 @@ const vowels = [..."iau"];
 const elements = ["ni", "ka", "fa", "su", "wa", "ga"];
 const defs = {
 	"pi": "part", "pigu": "meat, flesh", "piguka": "bone, wood", "piguga": "eye", "piguha": "mouth",
-		"pigunuhaku": "ear", "piguhaka": "tooth",
+		"pigunuhaku": "ear", "piguhaka": "tooth", "piguju": "breast", "pigujuku": "back",
 	"pu": "choose, or",
 	"ta": "have", "tajiju": "gain", "tajijuku": "lose",
 	"tu": "creation, and", "tuku": "destruction",
